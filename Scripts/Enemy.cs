2d3d415a0b84b467a478dca93c6d6de6f6e7aba1
DanielTvyt <daniel.voss@gmx.de>
2026-06-17 00:39:22 +0200

    Pathfinding

--- Scripts/Enemy.cs
+++ Scripts/Enemy.cs
@@ -5,17 +5,24 @@ using System.Runtime.InteropServices.JavaScript;
 public partial class Enemy : CharacterBody3D
 {
 	[Export]
-	private NavigationAgent3D _navigationAgent;
-
-	[Export]
 	float speed = 2.0f;
 
+	[Export]
+	Node3D target;
+
+	NavigationAgent3D _navigationAgent;	
 
     public override void _PhysicsProcess(double delta)
     {
-		Vector3 currentLocation = GlobalTransform.Origin;
+		_navigationAgent.TargetPosition = target.GlobalPosition;
+
+        Vector3 currentLocation = GlobalTransform.Origin;
 		Vector3 nextLocation = _navigationAgent.GetNextPathPosition();
 		Vector3 direction = (nextLocation - currentLocation).Normalized();
+
+        Velocity = direction * speed;
+
+        this.MoveAndSlide();
         base._PhysicsProcess(delta);
     }
 
@@ -23,7 +30,8 @@ public partial class Enemy : CharacterBody3D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-	}
+        _navigationAgent = GetNode<NavigationAgent3D>("NavigationAgent3D");
+    }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
